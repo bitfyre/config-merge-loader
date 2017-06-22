5c55a54ea9a6bd69e53e4ce4797793d63b56a14d
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-06-22 12:30:51 -0700

    DOH! forgot to update the test description

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var configMergeLoader = require('../index.js');
 
 describe('configMergeLoader', function() {
-  it('should return an object', function() {
+  it('should return an function', function() {
     assert.equal(typeof configMergeLoader, 'function');
   });
 });
