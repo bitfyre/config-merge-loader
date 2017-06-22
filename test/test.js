ee239ef4415590d899a71d8e42038a8bcd831f18
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-06-22 12:40:44 -0700

    Test that function returns and object

--- test/test.js
+++ test/test.js
@@ -5,4 +5,12 @@ describe('configMergeLoader', function() {
   it('should return an function', function() {
     assert.equal(typeof configMergeLoader, 'function');
   });
+
+  describe('the function', function() {
+    var mergedObj = configMergeLoader();
+
+    it('should return an object', function() {
+      assert.equal(typeof mergedObj, 'object');
+    });
+  });
 });
