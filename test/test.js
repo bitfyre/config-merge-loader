7877deb3ea3f2add6983c40c4f8171a9600a57d3
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-06-22 12:56:41 -0700

    Test what happens if only the default is defined

--- test/test.js
+++ test/test.js
@@ -7,10 +7,15 @@ describe('configMergeLoader', function() {
   });
 
   describe('the function', function() {
-    var mergedObj = configMergeLoader();
-
     it('should return an object', function() {
+      var mergedObj = configMergeLoader();
       assert.equal(typeof mergedObj, 'object');
+    });
+
+    it('when given only a default object, should just return that object', function() {
+        var defaultObj = { a: 1 };
+        var mergedDefault = configMergeLoader(defaultObj);
+        assert.equal(defaultObj, mergedDefault);
     });
   });
 });
