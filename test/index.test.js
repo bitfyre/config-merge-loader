0dd67adcfc35b225dff7548cfb0bd243ffc59e95
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-06-23 11:36:37 -0700

    Test that `configMergeLoader` returns a function

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const configMergeLoader = '';
+
+describe('Config Merge Loader', function() {
+  it('should return a function', function() {
+    assert.equal(typeof configMergeLoader, 'function');
+  });
+});
