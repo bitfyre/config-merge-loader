2d5d996f44d9ecbabecb7590de5a3de6fde7b318
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-06-25 18:57:42 -0700

    Test that when called, the loader returns a string

--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,13 @@ describe('Config Merge Loader', function() {
   it('should return a function', function() {
     assert.equal(typeof configMergeLoader, 'function');
   });
+
+  describe('when called', function() {
+    const loaderInput = 'Loader Content';
+
+    it('should return as string', function() {
+      const loaderResult = configMergeLoader(loaderInput);
+      assert.equal(typeof loaderResult, 'string');
+    });
+  });
 });
